db3013c6fa1706e53d8e8d4d3a5e06db3e0ee36e
Sho-Kawa0501 <akirasho0501@gmail.com>
2023-11-23 12:19:03 +0900

    rename10

--- reviewsite/settings.py
+++ reviewsite/settings.py
@@ -113,16 +113,32 @@ WSGI_APPLICATION = 'reviewsite.wsgi.application'
 #     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'stardb01',
-        'USER': 'postgres',
-        'PASSWORD': 'Akiradai51',
-        'HOST': 'localhost',
-        'PORT': '5432',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'stardb01',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Akiradai51',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'stardb01',
+            'USER': 'postgres',
+            'PASSWORD': 'Akiradai51',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
