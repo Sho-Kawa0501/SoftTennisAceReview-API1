ac5f83a976206b745d164998bc76c28aa849260c
Sho-Kawa0501 <akirasho0501@gmail.com>
2023-11-25 00:52:18 +0900

    deploy準備2

--- reviewsite/settings.py
+++ reviewsite/settings.py
@@ -106,17 +106,17 @@ TEMPLATES = [
     },
 ]
 
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
+# CORS_ALLOW_HEADERS = [
+#     'accept',
+#     'accept-encoding',
+#     'authorization',
+#     'content-type',
+#     'dnt',
+#     'origin',
+#     'user-agent',
+#     'x-csrftoken',
+#     'x-requested-with',
+# ]
 
 WSGI_APPLICATION = 'reviewsite.wsgi.application'
 
