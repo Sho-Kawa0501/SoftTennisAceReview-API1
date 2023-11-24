4d6fb901d90102c080f07babe603f631f6783461
Sho-Kawa0501 <akirasho0501@gmail.com>
2023-11-24 11:04:50 +0900

    deploy準備

--- reviewsite/settings.py
+++ reviewsite/settings.py
@@ -62,7 +62,9 @@ MIDDLEWARE = [
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:3000',
+    'https://soft-tennis-ace-review1-6eb3-git-release-sho-kawa0501.vercel.app/',
 ]
+
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 
@@ -71,6 +73,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'https://soft-tennis-ace-review1-6eb3-git-release-sho-kawa0501.vercel.app/',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -78,6 +81,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'https://soft-tennis-ace-review1-6eb3-git-release-sho-kawa0501.vercel.app/',
 ]
 
 ROOT_URLCONF = 'reviewsite.urls'
