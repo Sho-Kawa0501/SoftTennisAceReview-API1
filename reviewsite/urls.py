8d1be80b5960f9928223a8db5e84d00d45f4dd4c
Sho-Kawa0501 <akirasho0501@gmail.com>
2023-11-26 17:41:23 +0900

    urls.py mediapath

--- reviewsite/urls.py
+++ reviewsite/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenVerifyView
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.conf import settings
+from django.urls import re_path
 
 urlpatterns = [
     path('api/verify/', TokenVerifyView.as_view()),
@@ -10,10 +12,17 @@ urlpatterns = [
     path('api/item/',include('item.urls')),
     path('api/', include('review.urls')), 
     path('admin/', admin.site.urls),
+
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
+
+# if settings.DEBUG:
+#     urlpatterns += static(
+#         settings.MEDIA_URL,
+#         document_root=settings.MEDIA_ROOT
+#     )
