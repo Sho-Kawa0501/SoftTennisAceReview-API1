18523e701ccaa36995934edb2138bfb46675cf40
Sho-Kawa0501 <akirasho0501@gmail.com>
2023-12-01 22:31:28 +0900

    settings add STORAGES3

--- reviewsite/urls.py
+++ reviewsite/urls.py
@@ -12,21 +12,22 @@ urlpatterns = [
     path('api/item/',include('item.urls')),
     path('api/', include('review.urls')), 
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # urlpatterns += static(
 #     settings.MEDIA_URL,
 #     document_root=settings.MEDIA_ROOT
 # )
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+# if settings.DEBUG:
+#     urlpatterns += static(
+#         settings.MEDIA_URL, document_root=settings.STATIC_ROOT
+#     )
 
 # urlpatterns += [
 #     re_path(r'^media/(?P<path>.*)$', serve, {
-#         'document_root': settings.MEDIA_ROOT,
+#         'document_root': settings.STATIC_ROOT,
 #     }),
 # ]
 
