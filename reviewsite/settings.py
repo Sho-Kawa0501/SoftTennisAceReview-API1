2dbe42bc799b234640b5c2f8373f1fa6f0d26a59
Sho-Kawa0501 <akirasho0501@gmail.com>
2023-12-03 22:03:57 +0900

    settings add STORAGES30

--- reviewsite/settings.py
+++ reviewsite/settings.py
@@ -227,8 +227,8 @@ AUTH_USER_MODEL = 'account.CustomUser'
 
 
 # AWS-Settings
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_LOCATION = 'static' # s3バケット上のベースとなるファイルパス
