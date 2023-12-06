b2c1acecf787d0a8af652b0c68ba0fdb0916e0a7
Sho-Kawa0501 <akirasho0501@gmail.com>
2023-12-07 00:07:59 +0900

    cors path edit2

--- reviewsite/settings.py
+++ reviewsite/settings.py
@@ -86,7 +86,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
-    'https://www.softtennis-ace-review.com/',
+    'https://www.softtennis-ace-review.com',
 ]
 
 ROOT_URLCONF = 'reviewsite.urls'
