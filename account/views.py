fc7dd761fe55c438e6a4da7b0d9a4fe9013dfe67
Sho-Kawa0501 <akirasho0501@gmail.com>
2023-12-15 11:18:37 +0900

    中間テーブル作成 4

--- account/views.py
+++ account/views.py
@@ -240,8 +240,8 @@ class DeleteUserView(APIView):
 
         user_reviews = UserReview.objects.filter(user=user)
         for user_review in user_reviews:
-            if user_review.review.image:
-                delete_image_from_s3('reviews/' + user_review.review.image.name)
+          if user_review.review.image:
+            delete_image_from_s3('reviews/' + user_review.review.image.name)
 
         user.delete()
       return Response(status=status.HTTP_204_NO_CONTENT)
