0d87be78750a2473bc119fc289a931d7544fbd19
Sho-Kawa0501 <akirasho0501@gmail.com>
2023-12-19 18:38:30 +0900

    debug f

--- account/urls.py
+++ account/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('users/', include(router.urls)),
     path('token/refresh/',views.AccessTokenRefreshView.as_view(),name='access-token-refresh'),
     path('refresh-token/',views.RefreshGetView.as_view(),name='refresh-token'),
-    path('register/', views.RegisterView.as_view(),name='register'), 
+    path('register/', views.RegisterView.as_view(),name='register'),
     path('loginuser-information/', views.CheckAuthView.as_view(),name='loginuser-information'), 
     path('logout/',views.LogoutView.as_view(),name='logout'),
     path('user/delete/', views.DeleteUserView.as_view(),name='user/delete/'),
