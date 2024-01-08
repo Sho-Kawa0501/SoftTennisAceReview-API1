5c22010e934b86a113a06f0501b851410b03e194
Sho-Kawa0501 <akirasho0501@gmail.com>
2024-01-09 08:34:48 +0900

    edit account profile image views.py12

--- reviewsite/settings.py
+++ reviewsite/settings.py
@@ -19,8 +19,8 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'RENDER' not in os.environ
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
+# DEBUG = True
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY', default=os.environ['SECRET_KEY'])
